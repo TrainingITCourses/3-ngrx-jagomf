5b2c67e225e4193343a7b713a47fa49eebd29940
Jago <jago84@gmail.com>
2018-12-03 22:10:06 +0100

    Añadidos reducers para todas las entidades de la app

---
+++ speed/src/app/reducers/index.ts
@@ -0,0 +1,31 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import * as fromMission from './mission.reducer';
+import * as fromType from './type.reducer';
+import * as fromStatus from './status.reducer';
+import * as fromAgency from './agency.reducer';
+
+export interface State {
+
+  mission: fromMission.State;
+  type: fromType.State;
+  status: fromStatus.State;
+  agency: fromAgency.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+  mission: fromMission.reducer,
+  type: fromType.reducer,
+  status: fromStatus.reducer,
+  agency: fromAgency.reducer,
+};
+
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
